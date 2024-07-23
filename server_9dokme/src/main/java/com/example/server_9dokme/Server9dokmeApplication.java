6b44160e3ef6419aa923e6bed7f2ca270a3f4f43
KimMinHwan <127469367+rlaalsghks8@users.noreply.github.com>
2024-07-23 20:05:38 +0900

    BE:[FEAT] 프로젝트 생성 및 SwaggerConfig 추가

---
+++ server_9dokme/src/main/java/com/example/server_9dokme/Server9dokmeApplication.java
@@ -0,0 +1,13 @@
+package com.example.server_9dokme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Server9dokmeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Server9dokmeApplication.class, args);
+    }
+
+}
---
+++ server_9dokme/src/test/java/com/example/server_9dokme/Server9dokmeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.server_9dokme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Server9dokmeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
