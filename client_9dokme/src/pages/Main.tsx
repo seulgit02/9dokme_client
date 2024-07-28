ae5b61854fe66b4203b1ebc7ce8277d6ea9bf92a
seulgibaek <seulgi.baek@dgu.ac.kr>
2024-07-28 15:09:57 +0900

    FE:[FEAT] 메인페이지 광고배너 컴포너트 구현 #4

---
+++ client_9dokme/src/pages/Main.tsx
@@ -0,0 +1,10 @@
+import SlidingBanner from "../components/SlidingBanner.js";
+
+const Main = () => {
+  return (
+    <div className="w-screen h-screen bg-customColor bg-opacity-20">
+      <SlidingBanner />
+    </div>
+  );
+};
+export default Main;
---
+++ client_9dokme/tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        customColor: "#C5B5F7",
+      },
+      opacity: {
+        20: "0.2",
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+};
