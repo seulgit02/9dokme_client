133521301d285b6ae63d029f0a9f2a2a9f0b1e69
KimMinHwan <127469367+rlaalsghks8@users.noreply.github.com>
2024-07-30 15:44:53 +0900

    #2 BE:[FEAT] 로그아웃 구현

--- server_9dokme/src/main/java/com/example/server_9dokme/member/controller/MemberController.java
+++ server_9dokme/src/main/java/com/example/server_9dokme/member/controller/MemberController.java
@@ -9,15 +9,14 @@ import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
 import java.util.HashMap;
 
 @RestController
 @RequiredArgsConstructor
 @Slf4j
+@CrossOrigin
 @RequestMapping("/api")
 public class MemberController {
 
