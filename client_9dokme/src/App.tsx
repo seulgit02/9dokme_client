34c1f23ae24bcb68766333e91457179dee0635cb
chungchae <chungchae0@gmail.com>
2024-07-30 15:44:04 +0900

    developing

--- client_9dokme/src/App.tsx
+++ client_9dokme/src/App.tsx
@@ -3,10 +3,12 @@ import { Route, Routes } from "react-router-dom";
 import Main from "./pages/Main";
 import LoginPage from "./pages/Login";
 import LoginLoading from "./pages/LoginLoading";
+import Home from "./pages/Home";
 function App() {
   return (
     <Routes>
       <Route path='/' element={<Main />} />
+      <Route path='/home' element={<Home />} />
       <Route path='/logintest' element={<LoginPage />} />
       <Route
         path='/login/oauth2/callback/kakao'
