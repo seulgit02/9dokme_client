c92341b3d127eb12965e4e5182dac56a4dbe482e
chungchae <chungchae0@gmail.com>
2024-07-30 14:56:50 +0900

    #2 FE:{FEAT} 회원가입, 로그인

--- client_9dokme/src/App.tsx
+++ client_9dokme/src/App.tsx
@@ -1,10 +1,17 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Main from "./pages/Main";
+import LoginPage from "./pages/Login";
+import LoginLoading from "./pages/LoginLoading";
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Main />} />
+      <Route path='/' element={<Main />} />
+      <Route path='/logintest' element={<LoginPage />} />
+      <Route
+        path='/login/oauth2/callback/kakao'
+        element={<LoginLoading />} 
+      />
     </Routes>
   );
 }
--- client_9dokme/src/pages/Main.tsx
+++ client_9dokme/src/pages/Main.tsx
@@ -1,6 +1,21 @@
 import SlidingBanner from "../components/SlidingBanner";
 import Hashtag from "../components/Hashtag";
+import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
+import KakaoButton from "../components/KakaoButton";
 const Main = () => {
+  useEffect(() => {
+    // localStorage에 저장된 값을 모두 콘솔에 출력
+    const name = localStorage.getItem('name');
+    const email = localStorage.getItem('email');
+    console.log('Name:', name);
+    console.log('Email:', email);
+  }, []);
+  
+  const navigate = useNavigate();
+  const handleButtonClick = () => {
+    navigate(`/logintest`);
+  };
   return (
     <div className="w-screen h-full bg-customColor bg-opacity-20">
       <SlidingBanner />
@@ -13,6 +28,7 @@ const Main = () => {
           <Hashtag />
         </div>
       </div>
+      <KakaoButton onClick={handleButtonClick} label="로그인 페이지 이동" />
     </div>
   );
 };
