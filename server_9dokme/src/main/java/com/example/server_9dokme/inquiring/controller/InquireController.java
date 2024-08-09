54cb54d1c0232ad95fd347e9c637e71260b8c2b1
chungchae <chungchae0@gmail.com>
2024-08-09 23:22:48 +0900

    #3 BE:[FEAT] inquire 문의 기능

--- server_9dokme/src/main/java/com/example/server_9dokme/inquiring/controller/InquireController.java
+++ server_9dokme/src/main/java/com/example/server_9dokme/inquiring/controller/InquireController.java
@@ -1,11 +1,14 @@
 package com.example.server_9dokme.inquiring.controller;
 
+import com.example.server_9dokme.inquiring.dto.request.InquireRequestDto;
+import com.example.server_9dokme.inquiring.entity.Inquire;
+import com.example.server_9dokme.inquiring.service.InquireService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -13,4 +16,12 @@ import org.springframework.web.bind.annotation.RestController;
 @CrossOrigin(origins = "http://localhost:3000")
 @RequestMapping("/api")
 public class InquireController {
+    @Autowired
+    private InquireService inquireService;
+
+    @PostMapping
+    public ResponseEntity<Inquire> createInquire(@RequestBody InquireRequestDto inquireRequest) {
+        Inquire createdInquire = inquireService.createInquire(inquireRequest);
+        return new ResponseEntity<>(createdInquire, HttpStatus.CREATED);
+    }
 }
--- server_9dokme/src/main/java/com/example/server_9dokme/inquiring/service/InquireService.java
+++ server_9dokme/src/main/java/com/example/server_9dokme/inquiring/service/InquireService.java
@@ -1,9 +1,13 @@
 package com.example.server_9dokme.inquiring.service;
 
 
+import com.example.server_9dokme.inquiring.dto.request.InquireRequestDto;
+import com.example.server_9dokme.inquiring.entity.Inquire;
+import com.example.server_9dokme.inquiring.repository.InquireRepository;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Slf4j
@@ -11,4 +15,14 @@ import org.springframework.stereotype.Service;
 @NoArgsConstructor
 @Service
 public class InquireService {
+    @Autowired
+    private InquireRepository inquireRepository;
+
+    public Inquire createInquire(InquireRequestDto inquireRequest) {
+        Inquire inquire = new Inquire();
+        inquire.setTitle(inquireRequest.getTitle());
+        inquire.setContent(inquireRequest.getContent());
+        inquire.setUserId(inquireRequest.getUserId());
+        return inquireRepository.save(inquire);
+    }
 }
