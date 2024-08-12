460d78bffce93f59ea4df12661b6aa8ebe6a9ba3
bbabbi <mooncheayoung@gmail.com>
2024-08-12 14:43:17 +0900

    #17 BE : [FEAT] 북마크 해제 시, 북마크 아이디가 아닌 책 아이디를 통해 삭제되도록 변경(2)

--- server_9dokme/src/main/java/com/example/server_9dokme/bookmark/repository/BookmarkRepository.java
+++ server_9dokme/src/main/java/com/example/server_9dokme/bookmark/repository/BookmarkRepository.java
@@ -6,8 +6,10 @@ import com.example.server_9dokme.bookmark.entity.Bookmark;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface BookmarkRepository extends JpaRepository<Bookmark, Long> {
     Long countByBook(Book book);  // Book 엔티티 전체를 기반으로 카운트
     List<Bookmark> findByMember(Member member);  // Member를 기준으로 북마크 찾기
+    Optional<Bookmark> findByBookAndMember(Book book, Member member);
 }
