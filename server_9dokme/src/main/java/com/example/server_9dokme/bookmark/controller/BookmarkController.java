55fea412b2f25dad6615088ebd3321919c4314c3
bbabbi <mooncheayoung@gmail.com>
2024-08-12 14:42:22 +0900

    #17 BE : [CHORE] 북마크 등록에 대한 성공메시지 반환 x 데이터만 반환하도록 수정

--- server_9dokme/src/main/java/com/example/server_9dokme/bookmark/controller/BookmarkController.java
+++ server_9dokme/src/main/java/com/example/server_9dokme/bookmark/controller/BookmarkController.java
@@ -4,15 +4,12 @@ import com.example.server_9dokme.bookmark.service.BookmarkService;
 import com.example.server_9dokme.bookmark.service.dto.request.BookMarkRequest;
 import com.example.server_9dokme.bookmark.service.dto.request.BookUnMarkRequest;
 import com.example.server_9dokme.bookmark.service.dto.response.BookmarkResponse;
-import com.example.server_9dokme.common.dto.SuccessResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/api/bookmark")
 @RequiredArgsConstructor
 @Slf4j
 public class BookmarkController {
@@ -20,16 +17,14 @@ public class BookmarkController {
     private final BookmarkService bookmarkService;
 
     // 게시글 북마크 추가
-    @PostMapping("/bookmark")
-    public SuccessResponse<BookmarkResponse> addBookmark(@RequestBody BookMarkRequest bookMarkRequest) {
-        BookmarkResponse response = bookmarkService.mark(bookMarkRequest);
-        return SuccessResponse.success("북마크 반영 완료", response);
+    @PostMapping
+    public BookmarkResponse addBookmark(@RequestBody BookMarkRequest bookMarkRequest) {
+        return bookmarkService.mark(bookMarkRequest);
     }
 
     // 게시글 북마크 취소
-    @DeleteMapping("/bookmark")
-    public SuccessResponse<BookmarkResponse> removeBookmark(@RequestBody BookUnMarkRequest bookUnMarkRequest) {
-        BookmarkResponse response = bookmarkService.unmark(bookUnMarkRequest);
-        return SuccessResponse.success("북마크 해제 완료", response);
+    @DeleteMapping
+    public BookmarkResponse removeBookmark(@RequestBody BookUnMarkRequest bookUnMarkRequest) {
+        return bookmarkService.unmark(bookUnMarkRequest);
     }
 }
