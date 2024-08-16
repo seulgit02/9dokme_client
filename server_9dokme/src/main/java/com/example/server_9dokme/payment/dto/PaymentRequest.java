2bf7cee39a508fdef5487aacc0ccec7a8b48db75
bbabbi <mooncheayoung@gmail.com>
2024-08-16 16:43:04 +0900

    #6 BE : [FEAT] 결제 상태에 따른 결제 재요청 및 실패 처리(Spring Batch)

--- server_9dokme/src/main/java/com/example/server_9dokme/payment/dto/PaymentRequest.java
+++ server_9dokme/src/main/java/com/example/server_9dokme/payment/dto/PaymentRequest.java
@@ -1,19 +1,14 @@
 package com.example.server_9dokme.payment.dto;
 
-import lombok.Builder;
-import lombok.Data;
-
-@Data
-@Builder
-public class PaymentRequest {
-    private String pgProvider;
-    private String payMethod;
-    private String merchantUid;
-    private String name;
-    private int amount;
-    private String buyerEmail;
-    private String buyerName;
-    private String buyerTel;
-    private String buyerAddr;
-    private String buyerPostcode;
-}
+public record PaymentRequest(
+        String pgProvider,
+        String payMethod,
+        String merchantUid,
+        String name,
+        int amount,
+        String buyerEmail,
+        String buyerName,
+        String buyerTel,
+        String buyerAddr,
+        String buyerPostcode
+) {}
--- server_9dokme/src/main/java/com/example/server_9dokme/payment/repository/PaymentRepository.java
+++ server_9dokme/src/main/java/com/example/server_9dokme/payment/repository/PaymentRepository.java
@@ -2,8 +2,13 @@ package com.example.server_9dokme.payment.repository;
 
 import com.example.server_9dokme.payment.entity.Payment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface PaymentRepository extends JpaRepository<Payment, Long> {
     Payment findByImpUid(String impUid);
+    @Query("SELECT p FROM Payment p WHERE p.status = :status")
+    List<Payment> findAllByStatus(String status);
 }
 
