98cb01d0da6a1eab11bc3852672852e300503e33
seulgibaek <seulgi.baek@dgu.ac.kr>
2024-08-21 22:54:13 +0900

    FE:[FEAT] 북마크 기능 추가 구현

--- client_9dokme/src/components/BookCard.tsx
+++ client_9dokme/src/components/BookCard.tsx
@@ -1,63 +1,159 @@
-// BookCard.js (JavaScript로 변경)
+// // BookCard.js (JavaScript로 변경)
+// import React from "react";
+// import styled from "styled-components";
+// import { Card } from "antd";
+// import onBookmark from "../images/onBookmark.png";
+// import offBookmark from "../images/offBookmark.png";
+// import { useNavigate } from "react-router-dom";
+
+// const { Meta } = Card;
+
+// interface BookCardProps {
+//   cover: string; // 이미지 URL
+//   title: string; // 책 제목
+//   onClick: () => void; // 클릭 이벤트 핸들러
+//   isMarked: boolean;
+// }
+
+// const BookCard: React.FC<BookCardProps> = ({
+//   cover,
+//   title,
+//   isMarked,
+//   onClick,
+// }) => {
+//   return (
+//     <StyledCard hoverable onClick={onClick}>
+//       <BookmarkIcon
+//         src={isMarked ? onBookmark : offBookmark}
+//         alt="Bookmark Icon"
+//       />
+//       <img
+//         alt={title}
+//         src={cover}
+//         style={{ height: "16vw", borderRadius: "10px" }}
+//       />
+//       <Meta title={title.length > 11 ? `${title.slice(0, 11)}...` : title} />
+//     </StyledCard>
+//   );
+// };
+
+// const BookmarkIcon = styled.img`
+//   position: absolute;
+//   top: 10px;
+//   right: 10px;
+//   width: 30px;
+//   height: 30px;
+// `;
+
+// const StyledCard = styled(Card)`
+//   width: 13vw;
+//   height: 20vw;
+//   margin: 2vw;
+//   overflow: hidden;
+//   border-radius: 10px;
+
+//   img {
+//     border-radius: 10px;
+//     height: 16vw;
+//   }
+
+//   .ant-card-meta-title {
+//     text-align: center;
+//     font-size: 1.1vw;
+//     margin-bottom: 0.2vw;
+//     font-weight: bold;
+//   }
+
+//   &:before {
+//     content: "";
+//     position: absolute;
+//     top: 0;
+//     left: 0;
+//     width: 100%;
+//     height: 100%;
+//     background-color: #f0f0f0;
+//     border-radius: 10px;
+//     opacity: 0.1;
+//     z-index: -1;
+//     filter: blur(8px);
+//   }
+// `;
+
+// export default BookCard;
+
 import React from "react";
 import styled from "styled-components";
 import { Card } from "antd";
-import { useNavigate } from "react-router-dom";
+import onBookmark from "../images/onBookmark.png";
+import offBookmark from "../images/offBookmark.png";
 
 const { Meta } = Card;
 
 interface BookCardProps {
-  cover: string;  // 이미지 URL
-  title: string;  // 책 제목
-  onClick: () => void;  // 클릭 이벤트 핸들러
+  cover: string; // 이미지 URL
+  title: string; // 책 제목
+  onClick: () => void; // 클릭 이벤트 핸들러
+  isMarked: boolean; // 즐겨찾기 상태
 }
 
-const BookCard: React.FC<BookCardProps> = ({ cover, title, onClick }) => {
-  
-
-  //커버 이미지 연결 필요
+const BookCard: React.FC<BookCardProps> = ({
+  cover,
+  title,
+  isMarked,
+  onClick,
+}) => {
   return (
-    <StyledCard
-      hoverable
-      cover={<img alt={title} src={cover} />}
-      onClick={onClick}
-    >
-      <Meta title={title.length > 11 ? `${title.slice(0, 11)}...` : title} />
+    <StyledCard hoverable onClick={onClick}>
+      <CoverImageWrapper>
+        <CoverImage alt={title} src={cover} />
+        <BookmarkIcon
+          src={isMarked ? onBookmark : offBookmark}
+          alt="Bookmark Icon"
+        />
+      </CoverImageWrapper>
+      <Meta title={title.length > 15 ? `${title.slice(0, 15)}...` : title} />
     </StyledCard>
   );
 };
 
+const BookmarkIcon = styled.img`
+  position: absolute;
+  top: 10px;
+  right: 10px;
+  width: 30px;
+  height: 30px;
+`;
+
+const CoverImageWrapper = styled.div`
+  position: relative;
+  width: 100%;
+  height: 0;
+  padding-top: 150%;
+`;
+
+const CoverImage = styled.img`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+  border-radius: 10px 10px 0 0; // Rounded top corners only
+`;
+
 const StyledCard = styled(Card)`
-  width: 13vw;
-  height: 20vw;
-  margin: 2vw;
+  width: 100%;
+  background-color: transparent;
   overflow: hidden;
   border-radius: 10px;
-
-  img {
-    border-radius: 10px;
-    height: 16vw;
+  border-style: none;
+  .ant-card-body {
+    padding: 12px;
   }
-
   .ant-card-meta-title {
     text-align: center;
-    font-size: 1.1vw;
-    margin-bottom: 0.2vw;
+    font-size: large;
     font-weight: bold;
-  }
-
-  &:before {
-    content: '';
-    position: absolute;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-    background-color: #f0f0f0;
-    border-radius: 10px;
-    opacity: 0.1;
-    z-index: -1;
-    filter: blur(8px);
   }
 `;
 
