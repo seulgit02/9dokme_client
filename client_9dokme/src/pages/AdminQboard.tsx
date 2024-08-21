1fbb5b271e79cb2671de7c0f57cc4e296e341dd4
chungchae <chungchae0@gmail.com>
2024-08-21 21:21:18 +0900

    FE:[FEAT] 문의게시판 연결

--- client_9dokme/src/pages/AdminQboard.tsx
+++ client_9dokme/src/pages/AdminQboard.tsx
@@ -2,7 +2,7 @@ import styled from "styled-components";
 import AdminBanner from "../components/AdminBanner";
 import QueryData from "../json/Query.json";
 import { Inquiry, InquiryResponse, SortInfo, Pageable } from "../json/Query";
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import QueryDetail from "../components/QueryDetail";
 import API from "../api/axios";
 import {
@@ -15,18 +15,25 @@ import {
   TableCell,
   TableCaption,
 } from "../components/ui/table";
-import axios from 'axios';
-import ReactPaginate from 'react-paginate';
+import axios from "axios";
+import ReactPaginate from "react-paginate";
+import styles from './AdminQBoard.module.css';
+
 
 const AdminQboard = () => {
   const [openEditor, setOpenEditor] = useState<boolean>(false);
   const [selectedQuery, setSelectedQuery] = useState<Inquiry | null>(null);
-  const [queryData, setQueryData] = useState<any>({ content: [], totalPages: 0 });
+  const [queryData, setQueryData] = useState<any>({
+    content: [],
+    totalPages: 0,
+  });
   const [currentPage, setCurrentPage] = useState<number>(0);
 
   const fetchQueries = async (pageNo: number) => {
     try {
-      const response = await axios.get(`http://localhost:8080/api/admin/inquiries/${pageNo}`);
+      const response = await axios.get(
+        `http://localhost:8080/api/admin/inquiries/${pageNo}`
+      );
       setQueryData(response.data);
       console.log(response.data);
     } catch (error) {
@@ -71,36 +78,36 @@ const AdminQboard = () => {
   };
 
   return (
-    <div className="flex-col h-screen bg-customColor bg-opacity-20 flex justify-center items-center">
+    <div className='flex-col h-screen bg-customColor bg-opacity-20 flex justify-center items-center'>
       {openEditor && selectedQuery && (
         <QueryDetail query={selectedQuery} onBack={onBack} />
       )}
 
       <AdminBanner />
-      <h1 className="font-bold">문의게시판 관리 페이지</h1>
-      <Table className="bg-white m-[3vw] w-[60vw] ml-[20vw]">
+      <h1 className='font-bold'>문의게시판 관리 페이지</h1>
+      <Table className='bg-white m-[3vw] w-[60vw] ml-[20vw]'>
         <TableHeader>
-          <TableRow className="bg-[#D8E7FF]">
-            <TableHead className="w-[10vw]">문의글 제목</TableHead>
-            <TableHead className="w-[30vw]">문의글 내용</TableHead>
-            <TableHead className="w-[10vw]">조회</TableHead>
-            <TableHead className="w-[7vw]">삭제</TableHead>
+          <TableRow className='bg-[#D8E7FF]'>
+            <TableHead className='w-[10vw]'>문의글 제목</TableHead>
+            <TableHead className='w-[30vw]'>문의글 내용</TableHead>
+            <TableHead className='w-[10vw]'>조회</TableHead>
+            <TableHead className='w-[7vw]'>삭제</TableHead>
           </TableRow>
         </TableHeader>
         <TableBody>
           {queryData.content.map((query: Inquiry) => (
-            <TableRow key={query.inquireId} className="text-left">
-              <TableCell className="font-medium">{query.title}</TableCell>
-              <TableCell className="font-medium">
-                {query.content.length > 20
-                  ? query.content.slice(0, 20) + "..."
+            <TableRow key={query.inquireId} className='text-left'>
+              <TableCell className='font-medium'>{query.title}</TableCell>
+              <TableCell className='font-medium'>
+                {query.content.length > 40
+                  ? query.content.slice(0, 40) + "..."
                   : query.content}
               </TableCell>
 
               <TableCell>
                 <button
                   onClick={() => detailViewClick(query)}
-                  className="bg-slate-400 text-[0.8vw] text-white px-[0.5vw] rounded hover:bg-customColor3"
+                  className='bg-slate-400 text-[0.8vw] text-white px-[0.5vw] rounded hover:bg-customColor3'
                 >
                   상세조회
                 </button>
@@ -108,7 +115,7 @@ const AdminQboard = () => {
               <TableCell>
                 <button
                   onClick={() => onDeleteClick(String(query.inquireId))}
-                  className="bg-slate-400 text-[0.8vw] text-white px-[0.5vw] rounded hover:bg-[#FF7E7E]"
+                  className='bg-slate-400 text-[0.8vw] text-white px-[0.5vw] rounded hover:bg-[#FF7E7E]'
                 >
                   삭제
                 </button>
@@ -118,32 +125,30 @@ const AdminQboard = () => {
         </TableBody>
       </Table>
 
-<PageContainer>
-      <ReactPaginate
-        previousLabel={"이전"}
-        nextLabel={"다음"}
-        breakLabel={"..."}
-        pageCount={queryData.totalPages}
-        marginPagesDisplayed={2}
-        pageRangeDisplayed={5}
-        onPageChange={handlePageClick}
-        containerClassName={"pagination"}
-        pageClassName={"page-item"}
-        pageLinkClassName={"page-link"}
-        previousClassName={"page-item"}
-        previousLinkClassName={"page-link"}
-        nextClassName={"page-item"}
-        nextLinkClassName={"page-link"}
-        breakClassName={"page-item"}
-        breakLinkClassName={"page-link"}
-        activeClassName={"active"}
-      />
+      <PageContainer>
+        <ReactPaginate
+          previousLabel={"이전"}
+          nextLabel={"다음"}
+          breakLabel={"..."}
+          pageCount={queryData.totalPages}
+          marginPagesDisplayed={2}
+          pageRangeDisplayed={5}
+          onPageChange={handlePageClick}
+          containerClassName={styles.pagination}
+          pageClassName={styles.pageItem}
+          pageLinkClassName={styles.pageLink}
+          previousClassName={styles.pageItem}
+          previousLinkClassName={styles.pageLink}
+          nextClassName={styles.pageItem}
+          nextLinkClassName={styles.pageLink}
+          breakClassName={styles.pageItem}
+          breakLinkClassName={styles.pageLink}
+          activeClassName={styles.active}
+        />
       </PageContainer>
     </div>
   );
-  
 };
-
 
 const Box = styled.div`
   width: 75vw;
@@ -160,9 +165,8 @@ const Box = styled.div`
 `;
 const PageContainer = styled.div`
   align-items: center;
-  display:flex;
+  display: flex;
   flex-direction: row;
-`
-
+`;
 
 export default AdminQboard;
