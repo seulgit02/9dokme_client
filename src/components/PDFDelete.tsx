093446f49eb54c1f10c0af08838120180cee09e9
seulgibaek <seulgi.baek@dgu.ac.kr>
2024-08-22 11:32:41 +0900

    FE:[FEAT] 관리자페이지-DPF 삭제 API 연동

--- src/components/PDFDelete.tsx
+++ src/components/PDFDelete.tsx
@@ -1,45 +1,70 @@
+import React, { useState } from "react";
 import styled from "styled-components";
 import BookDetail from "../json/BookDetail.json";
-import { BookDetailType, Books } from "../json/BookDetailType";
+import { BookDetailType } from "../json/BookDetailType";
+import API from "../api/axios"; // API 호출을 위한 axios 인스턴스
+
 import {
   Table,
   TableHeader,
   TableBody,
-  TableFooter,
-  TableHead,
   TableRow,
+  TableHead,
   TableCell,
-  TableCaption,
 } from "./ui/table";
 
-const PDFDelete: React.FC = () => (
-  <Box>
-    <Table className="bg-white m-[3vw]">
-      <TableHeader>
-        <TableRow className="bg-[#D8E7FF]">
-          <TableHead className="w-[30vw]">책 제목</TableHead>
-          <TableHead className="w-[20vw]">저자</TableHead>
-          <TableHead className="w-[13vw]">등록일</TableHead>
-          <TableHead className="w-[7vw]">선택</TableHead>
-        </TableRow>
-      </TableHeader>
-      <TableBody>
-        {BookDetail.books.map((book: BookDetailType) => (
-          <TableRow key={book.bookId} className="text-left">
-            <TableCell className="font-medium">{book.bookTitle}</TableCell>
-            <TableCell>{book.author}</TableCell>
-            <TableCell>{book.publishDate}</TableCell>
-            <TableCell>
-              <button className="bg-slate-400 text-[0.8vw] text-white px-[0.5vw] rounded hover:bg-[#FF7E7E]">
-                삭제
-              </button>
-            </TableCell>
+const PDFDelete: React.FC = () => {
+  const [books, setBooks] = useState<BookDetailType[]>(BookDetail.books);
+
+  const handleDelete = async (bookId: number) => {
+    if (window.confirm("정말로 이 책을 삭제하시겠습니까?")) {
+      try {
+        await API.delete(`/admin/books/${bookId}`);
+        alert("책이 성공적으로 삭제되었습니다.");
+
+        // 삭제된 책을 books 상태에서 제거
+        setBooks((prevBooks) =>
+          prevBooks.filter((book) => book.bookId !== bookId)
+        );
+      } catch (error) {
+        alert("책 삭제에 실패하였습니다.");
+        console.error("삭제 실패:", error);
+      }
+    }
+  };
+
+  return (
+    <Box>
+      <Table className="bg-white m-[3vw]">
+        <TableHeader>
+          <TableRow className="bg-[#D8E7FF]">
+            <TableHead className="w-[30vw]">책 제목</TableHead>
+            <TableHead className="w-[20vw]">저자</TableHead>
+            <TableHead className="w-[13vw]">등록일</TableHead>
+            <TableHead className="w-[7vw]">선택</TableHead>
           </TableRow>
-        ))}
-      </TableBody>
-    </Table>
-  </Box>
-);
+        </TableHeader>
+        <TableBody>
+          {books.map((book: BookDetailType) => (
+            <TableRow key={book.bookId} className="text-left">
+              <TableCell className="font-medium">{book.bookTitle}</TableCell>
+              <TableCell>{book.author}</TableCell>
+              <TableCell>{book.publishDate}</TableCell>
+              <TableCell>
+                <button
+                  className="bg-slate-400 text-[0.8vw] text-white px-[0.5vw] rounded hover:bg-[#FF7E7E]"
+                  onClick={() => handleDelete(book.bookId)}
+                >
+                  삭제
+                </button>
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </Box>
+  );
+};
 
 const Box = styled.div`
   width: 75vw;
